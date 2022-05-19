c1f5412826e8c197de23f79d2875456a1bba1883
Naz Quadri <naz.quadri@gmail.com>
2022-05-19 10:30:15 -0400

    sync to docs fusion_release branch

--- fusion/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Top-level package for fusion."""
-
-__author__ = """Fusion Devs"""
-__email__ = 'fusion_developers@jpmorgan.com'
-__version__ = '1.0.4'
-
-from fusion.fusion import Fusion, FusionCredentials
