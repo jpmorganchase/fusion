b20f26a0936a3f6f928bd2337fbab916d603ee1b
dbernaciak <dawid.bernaciak@gmail.com>
2022-09-13 17:00:14 +0100

    delete redundant code

--- fusion/fusion_filesystem.py
+++ fusion/fusion_filesystem.py
@@ -9,23 +9,6 @@ logger = logging.getLogger(__name__)
 VERBOSE_LVL = 25
 
 
-async def _ls_real(self, url, detail=True, **kwargs):
-    # ignoring URL-encoded arguments
-    clean_url = url
-    if "http" not in url:
-        url = f'{self.client_kwargs["root_url"]}catalogs/' + url
-    kw = self.kwargs.copy()
-    kw.update(kwargs)
-    # logger.debug(url)
-    session = await self.set_session()
-    async with session.get(url, **self.kwargs) as r:
-        self._raise_not_found_for_status(r, url)
-        out = await r.json()
-
-    out = [clean_url + f'/{x["identifier"]}' for x in out["resources"]]
-    return out
-
-
 class FusionHTTPFileSystem(HTTPFileSystem):
     """Fusion HTTP filesystem.
     """
@@ -231,7 +214,7 @@ class FusionHTTPFileSystem(HTTPFileSystem):
             rpath,
             chunk_size=5 * 2 ** 20,
             callback=_DEFAULT_CALLBACK,
-            method="post",
+            method="put",
             **kwargs):
         """
         Copy file(s) from local.
