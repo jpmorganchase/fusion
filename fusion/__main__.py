6a5602dd7caf91a9697377d9384f69754ac3c765
dbernaciak <dawid.bernaciak@gmail.com>
2023-03-18 23:16:30 -0400

    mypy fix

--- fusion/__main__.py
+++ fusion/__main__.py
@@ -10,19 +10,19 @@ if __name__ == "__main__":
                callable(getattr(Fusion, method_name)) and not method_name.startswith("_")]
 
     args = inspect.signature(Fusion.__init__).parameters
-    args = {param.name for param in args.values()}
+    args = {param.name for param in args.values()}  # type: ignore
 
     for m in methods:
         met = getattr(Fusion, m)
-        args = args.union({param.name for param in inspect.signature(met).parameters.values()})
+        args = args.union({param.name for param in inspect.signature(met).parameters.values()})  # type: ignore
 
-    args = args.difference({"self"})
+    args = args.difference({"self"})  # type: ignore
 
     for a in args:
         parser.add_argument('--'+a, default=None)
 
     parser.add_argument('--method', default=None)
-    args = parser.parse_args()
+    args = parser.parse_args()  # type: ignore
     kw = {}
     for k in ["root_url", "credentials", "download_folder", "log_level", "log_path"]:
         if vars(args)[k] is not None:
