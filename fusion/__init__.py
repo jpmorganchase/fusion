2f8618bf53dcd95a815190769570e619c4ae1dde
Dawid Bernaciak <68283272+dbernaciak@users.noreply.github.com>
2024-04-12 16:54:22 -0400

    prepare release

--- fusion/__init__.py
+++ fusion/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Fusion Devs"""
 __email__ = "fusion_developers@jpmorgan.com"
-__version__ = "1.0.22"
+__version__ = "1.0.23"
 
 from fusion.fs_sync import fsync
 from fusion.fusion import Fusion, FusionCredentials
