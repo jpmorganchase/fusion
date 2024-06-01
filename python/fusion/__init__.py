2b13dd6082a64e946dd27df3aa67537684fc8735
Dawid Bernaciak <68283272+dbernaciak@users.noreply.github.com>
2024-06-01 11:21:36 -0400

    Feature/project structure (#50)
    * refactor project structure to allow mixed Rust/Python

--- python/fusion/__init__.py
+++ python/fusion/__init__.py
@@ -4,10 +4,10 @@ __author__ = """Fusion Devs"""
 __email__ = "fusion_developers@jpmorgan.com"
 __version__ = "1.1.0.dev3"
 
-from _fusion import *  # noqa: F403
-
 from fusion.authentication import FusionCredentials
 from fusion.fs_sync import fsync
 from fusion.fusion import Fusion
+
+from ._fusion import *  # noqa: F403
 
 __all__ = ["Fusion", "FusionCredentials", "fsync", "rust_ok"]  # noqa: F405
