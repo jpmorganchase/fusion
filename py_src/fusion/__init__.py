79688b51ac49a7fc0f52ea042d7145be7b15c6f0
Dawid Bernaciak <dawid.bernaciak@jpmorgan.com>
2024-08-01 17:03:01 -0400

    auth fix

--- py_src/fusion/__init__.py
+++ py_src/fusion/__init__.py
@@ -2,11 +2,7 @@
 
 __author__ = """Fusion Devs"""
 __email__ = "fusion_developers@jpmorgan.com"
-<<<<<<< HEAD
 __version__ = "1.2.2"
-=======
-__version__ = "1.2.1"
->>>>>>> origin/master
 
 from fusion._fusion import FusionCredentials
 from fusion.fs_sync import fsync
