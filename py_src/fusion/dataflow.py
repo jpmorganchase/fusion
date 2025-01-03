726a5a53ae6404931bbf018c0a0fd39ee9f59969
Georgianna James <georgianna.james@jpmchase.com>
2025-01-03 16:11:58 -0500

    update obj repr

--- py_src/fusion/dataflow.py
+++ py_src/fusion/dataflow.py
@@ -12,6 +12,16 @@ class InputDataFlow(Dataset):
     flowDetails: dict[str, str] | None = None
     type_: str = field(default="Flow", init=False)
 
+    def __repr__(self: InputDataFlow) -> str:
+        """Return an object representation of the InputDataFlow object.
+
+        Returns:
+            str: Object representation of the dataset.
+
+        """
+        attrs = {k: v for k, v in self.__dict__.items() if not k.startswith("_")}
+        return f"InputDataFlow(\n" + ",\n ".join(f"{k}={v!r}" for k, v in attrs.items()) + "\n)"
+
 
 @dataclass
 class OutputDataFlow(Dataset):
@@ -19,3 +29,13 @@ class OutputDataFlow(Dataset):
     consumerApplicationId: dict[str, str] | None = None
     flowDetails: dict[str, str] | None = None
     type_: str = field(default="Flow", init=False)
+
+    def __repr__(self: OutputDataFlow) -> str:
+        """Return an object representation of the OutputDataFlow object.
+
+        Returns:
+            str: Object representation of the dataset.
+
+        """
+        attrs = {k: v for k, v in self.__dict__.items() if not k.startswith("_")}
+        return f"OutputDataFlow(\n" + ",\n ".join(f"{k}={v!r}" for k, v in attrs.items()) + "\n)"
--- py_src/fusion/report.py
+++ py_src/fusion/report.py
@@ -14,4 +14,14 @@ class Report(Dataset):
         super().__post_init__()
         if not self.report:
             raise ValueError("The 'report' attribute is required and cannot be empty.")
+        
+    def __repr__(self: Report) -> str:
+        """Return an object representation of the Report object.
+
+        Returns:
+            str: Object representation of the dataset.
+
+        """
+        attrs = {k: v for k, v in self.__dict__.items() if not k.startswith("_")}
+        return f"Report(\n" + ",\n ".join(f"{k}={v!r}" for k, v in attrs.items()) + "\n)"
 
