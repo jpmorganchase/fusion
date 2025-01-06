c5f9f08a9807726c90adbbffc17878cd66ce36fa
Georgianna James <georgianna.james@jpmchase.com>
2025-01-06 13:20:44 -0500

    start dataflow test file

---
+++ py_tests/test_dataflow.py
@@ -0,0 +1,15 @@
+"""Test dataflow.py"""
+
+from fusion.dataflow import InputDataFlow, OutputDataFlow
+
+
+def test_inputdataflow_class_object_representation() -> None:
+    """Test the object representation of the Dataflow class."""
+    dataflow = InputDataFlow(identifier="my_dataflow", flowDetails={"key": "value"})
+    assert repr(dataflow)
+
+def test_outputdataflow_class_object_representation() -> None:
+    """Test the object representation of the Dataflow class."""
+    dataflow = OutputDataFlow(identifier="my_dataflow", flowDetails={"key": "value"})
+    assert repr(dataflow)
+
