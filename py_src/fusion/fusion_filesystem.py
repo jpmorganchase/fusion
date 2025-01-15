0173e3b78cb637414592bbbdb6d526989eb4a06c
Georgianna James <georgianna.james@jpmchase.com>
2025-01-15 10:13:39 -0500

    download fix

--- py_src/fusion/fusion_filesystem.py
+++ py_src/fusion/fusion_filesystem.py
@@ -23,7 +23,7 @@ from fsspec.utils import nullcontext
 
 from fusion._fusion import FusionCredentials
 
-from .utils import get_client, get_default_fs, get_session
+from .utils import cpu_count, get_client, get_default_fs, get_session
 
 logger = logging.getLogger(__name__)
 VERBOSE_LVL = 25
@@ -508,7 +508,7 @@ class FusionHTTPFileSystem(HTTPFileSystem):  # type: ignore
             lpath = default_fs.open(lpath, "wb")
 
         rpath = self._decorate_url(rpath) if isinstance(rpath, str) else rpath
-        n_threads = kwargs.get("n_threads", 1)
+        n_threads = cpu_count(is_threading=True)
         file_size = None
         if "headers" in kwargs and "Content-Length" in kwargs["headers"]:
             file_size = int(kwargs["headers"].get("Content-Length"))
