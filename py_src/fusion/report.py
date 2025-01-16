9412557965fb5655495acce031c2cb58efc2d430
Georgianna James <97406156+georgiannajames@users.noreply.github.com>
2025-01-16 09:14:21 -0500

    Feature/bcbs (#96)
    * bcbs updates

---
+++ py_src/fusion/report.py
@@ -0,0 +1,73 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from typing import TYPE_CHECKING
+
+from fusion.dataset import Dataset
+from fusion.utils import requests_raise_for_status
+
+if TYPE_CHECKING:
+    import requests
+
+    from fusion.fusion import Fusion
+
+
+@dataclass
+class Report(Dataset):
+    """Fusion Report class for managing regulatory reporting metadata.
+    
+    Attributes:
+        report (dict[str, str]): The report metadata. Specifies the tier of the report.
+        type_ (str): The dataset type. Defaults to "Report", which is the required value for creating a Report object.
+
+    """
+    report: dict[str, str] | None = field(default_factory=lambda: {"tier": ""})
+    type_: str | None = "Report"
+        
+    def __repr__(self: Report) -> str:
+        """Return an object representation of the Report object.
+
+        Returns:
+            str: Object representation of the dataset.
+
+        """
+        attrs = {k: v for k, v in self.__dict__.items() if not k.startswith("_")}
+        return f"Report(\n" + ",\n ".join(f"{k}={v!r}" for k, v in attrs.items()) + "\n)"
+    
+    def add_registered_attribute(
+        self: Report,
+        attribute_identifier: str,
+        is_key_data_element: bool,
+        catalog: str | None = None,
+        client: Fusion | None = None,
+        return_resp_obj: bool = False,
+    ) -> requests.Response | None:
+        """Add a registered attribute to the Report.
+
+        Args:
+            attribute_identifier (str): Attribute identifier.
+            is_key_data_element (bool): Key Data Element flag. An attribute can be proposed as a key data element when
+                it is linked to a report. This property is specific to the relationship between the attribute and the
+                report.
+            catalog (str | None, optional): Catalog identifier. Defaults to 'common'.
+            client (Fusion, optional): A Fusion client object. Defaults to the instance's _client.
+                If instantiated from a Fusion object, then the client is set automatically.
+            return_resp_obj (bool, optional): If True then return the response object. Defaults to False.
+
+        Returns:
+            requests.Response | None: The response object from the API call if return_resp_obj is True, otherwise None.
+        """
+        client = self._use_client(client)
+        catalog = client._use_catalog(catalog)
+        dataset = self.identifier
+
+        url = f"{client.root_url}catalogs/{catalog}/datasets/{dataset}/attributes/{attribute_identifier}/registration"
+
+        data = {
+            "isCriticalDataElement": is_key_data_element,
+        }
+
+        resp: requests.Response = client.session.post(url, json=data)
+        requests_raise_for_status(resp)
+
+        return resp if return_resp_obj else None
---
+++ py_tests/test_dataflow.py
@@ -0,0 +1,39 @@
+"""Test dataflow.py"""
+
+import requests
+import requests_mock
+
+from fusion.dataflow import InputDataFlow, OutputDataFlow
+from fusion.fusion import Fusion
+
+
+def test_inputdataflow_class_object_representation() -> None:
+    """Test the object representation of the Dataflow class."""
+    dataflow = InputDataFlow(identifier="my_dataflow", flow_details={"key": "value"})
+    assert repr(dataflow)
+
+def test_outputdataflow_class_object_representation() -> None:
+    """Test the object representation of the Dataflow class."""
+    dataflow = OutputDataFlow(identifier="my_dataflow", flow_details={"key": "value"})
+    assert repr(dataflow)
+
+
+def test_add_registered_attribute(requests_mock: requests_mock.Mocker, fusion_obj: Fusion) -> None:
+    """Test the add_registered_attribute method."""
+    catalog = "my_catalog"
+    dataflow = "TEST_DATAFLOW"
+    attribute_identifier = "my_attribute"
+    url = f"{fusion_obj.root_url}catalogs/{catalog}/datasets/{dataflow}/attributes/{attribute_identifier}/registration"
+
+    requests_mock.post(url, json={"isCriticalDataElement": False})
+
+    dataflow_obj = InputDataFlow(identifier="TEST_DATAFLOW")
+    dataflow_obj.client = fusion_obj
+    resp = dataflow_obj.add_registered_attribute(
+        attribute_identifier="my_attribute",
+        catalog=catalog,
+        return_resp_obj=True
+    )
+    assert isinstance(resp, requests.Response)
+    status_code = 200
+    assert resp.status_code == status_code
