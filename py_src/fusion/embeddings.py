7637dbf9520fc9e31e0192257759c94b9b0125ba
Georgianna James <georgianna.james@jpmchase.com>
2025-03-05 15:02:17 -0500

    fix timeout arg issue

--- py_src/fusion/embeddings.py
+++ py_src/fusion/embeddings.py
@@ -634,7 +634,7 @@ class FusionAsyncHttpConnection(AIOHttpConnection):  # type: ignore
                 data=body,
                 auth=auth,
                 headers=req_headers,
-                timeout=aiohttp.ClientTimeout(total=timeout) if timeout is not None else None,
+                timeout=timeout,  # type: ignore
             ) as response:
                 raw_data = await response.text()
                 duration = self.loop.time() - start
