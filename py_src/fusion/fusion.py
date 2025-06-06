48013d1aa159212b9baf5170354c22a0fb1ce959
Pushpak Manwani <pushpak.manwani@jpmchase.com>
2025-06-06 15:32:46 +0100

    clean up

--- py_src/fusion/fusion.py
+++ py_src/fusion/fusion.py
@@ -10,7 +10,7 @@ import sys
 import warnings
 from io import BytesIO
 from pathlib import Path
-from typing import TYPE_CHECKING, Any, TypedDict
+from typing import TYPE_CHECKING, Any
 from zipfile import ZipFile
 
 import pandas as pd
